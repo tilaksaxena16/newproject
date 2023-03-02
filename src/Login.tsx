bf38bdc7ae38d175e9782147fc743368d1970b8f
Your Name <you@example.com>
2023-03-02 23:25:30 +0530

    new project

---
+++ src/Login.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+const Login :React.FC = () => {
+
+    return (
+        <>
+            <h1>hello</h1>
+        </>
+    )
+};
+
+export { Login };
---
+++ src/Regis.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+const Regis: React.FC = () => {
+
+    return (
+        <>
+            <h1>World</h1>
+        </>
+    )
+};
+
+export { Regis };
